e746db278deabceb82fde24e5ed15211010f6861
CY <onions.yang at gmail.com>
2016-09-05 21:25:52 +0800

    I don't remember what changed ...

---
+++ lib/real_estate/property_guru.rb
@@ -0,0 +1,10 @@
+module Realestate
+  class PropertyGuru
+    HOST = "http://www.propertyguru.com.sg/"
+    url = "/singapore-property-listing/property-for-sale?property_id=201&district_code=D15&market=residential&freetext=Imperial+Heights"
+
+    def self.districts
+      
+    end
+  end
+end
