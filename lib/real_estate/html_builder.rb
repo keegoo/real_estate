75693e95a55ec0fbfe9cf3ca277a177ad0a946a7
CY <onions.yang@gmail.com>
2017-02-12 08:45:43 +0800

    add custom HEADERS and sleep(rand) to http get

--- lib/real_estate/html_builder.rb
+++ lib/real_estate/html_builder.rb
@@ -4,6 +4,15 @@ require 'cgi'
 module RealEstate
   class HTMLBuilder
     ROOT = 'http://www.propertyguru.com.sg'
+
+    HEADERS = {
+      "Connection" => "keep-alive",
+      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+      "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Encoding" => "gzip, deflate, sdch",
+      "Accept-Language" => "en-US,en;q=0.8,fr;q=0.6,zh-CN;q=0.4,zh;q=0.2,zh-TW;q=0.2"
+    }
+
     def self.districts
       HTMLBuilder.get(ROOT + "/")
     end
@@ -16,8 +25,7 @@ module RealEstate
     #   "district_code[]" => "D28"
     # }
     def self.pages_count(district)
-      query = query_str(district)
-
+      query = HTMLBuilder.query_str(district)
       HTMLBuilder.get(ROOT + "/singapore-property-listing/property-for-sale?" + query)
     end
 
@@ -27,8 +35,8 @@ module RealEstate
     # 
     # page = 1
     def self.page(district, page)
-      query = query_str(district)
-
+      raise "variable: district, must be a hash" unless district.kind_of?(Hash)
+      query = HTMLBuilder.query_str(district)
       HTMLBuilder.get(ROOT + "/singapore-property-listing/property-for-sale/#{page}?" + query)
     end
 
@@ -38,15 +46,18 @@ module RealEstate
 
     private
 
-    def self.query_str(hash)
-      hash.map{|k, v| "#{CGI::escape(k)}=#{CGI::escape(v)}"}.join("&")
+    def self.query_str(ahash)
+      ahash.map{|k, v| "#{CGI::escape(k)}=#{CGI::escape(v)}"}.join("&")
     end
 
     def self.get(url)
+      sleep(Random.new.rand(3..5))
+
       if RealEstate::FIDDLER_PROXY_ENABLED
         options = {
           http_proxyaddr: "127.0.0.1",
-          http_proxyport: "8888"
+          http_proxyport: "8888",
+          headers: HEADERS
         }
         HTTParty.get(url, options)
       else
