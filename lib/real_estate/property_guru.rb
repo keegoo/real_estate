91fe86ecc06704a4daf49b6ea49591205aeaa08e
CY <onions.yang@gmail.com>
2017-02-13 21:21:10 +0800

    add property_ids function to replace properties function

--- lib/real_estate/property_guru.rb
+++ lib/real_estate/property_guru.rb
@@ -37,16 +37,13 @@ module RealEstate
 
     # ------------------
     # get properties in specific page of district
-    def properties(district, page_num)
-      x = []
+    def property_ids(district, page_num)
       doc = Nokogiri::HTML(@html.page(district, page_num))
-      doc.css('div.listing-list li.listing-item').each do |property_node|
-        # puts property_node.inner_html
-        # exit
-        ppt = Property.get_property_from_html(property_node.inner_html)
-        x.push(ppt)
-      end
-      x
+
+      doc.css('li.listing-item').
+        map{|x|x["class"]}.               # ["listing-item  listing-id-20461748 ", "listing-item featured-agent-listing listing-id-18743687 ", ...]
+        map{|x|x.strip.split(" ")[-1]}.   # ["listing-id-20461748", "listing-id-18743687"]
+        map{|x|x.split("-")[-1]}          # ["20461748", "18743687"]
     end
   end
 end
